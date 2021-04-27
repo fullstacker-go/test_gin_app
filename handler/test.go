965eb9a088f9e4d5e48c361ab9ce8447d1245e17
tanvirs <tanvirs@mkcl.org>
2021-04-27 13:30:47 +0530

    seperated handler logic from main package and declared it inside its own package

--- handler/test.go
+++ handler/test.go
@@ -5,26 +5,27 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/fullstacker-go/concurr_gin/model"
 	"github.com/gin-gonic/gin"
 )
 
-func responseSize(url string, channel chan int) {
-	fmt.Println("Getting", url)              // Unchanged
-	response, _ := http.Get(url)             // Unchanged
+func responseSize(url string, channel chan model.WebStats) {
+	fmt.Println("Getting", url) // Unchanged
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Returning...", err)
+		return
+	}
 	defer response.Body.Close()              // Unchanged
 	body, _ := ioutil.ReadAll(response.Body) // Unchanged
 	// Send body length value via channel.
-	channel <- len(body)
+	channel <- model.WebStats{url, len(body)}
 }
 func TestHandler(c *gin.Context) {
-	size := make(chan int)
-	resp_godoc := make(chan int)
-	resp_exm := make(chan int)
-	go responseSize("https://golang.org/", size)
-	go responseSize("https://golang.org/doc", resp_godoc)
-	go responseSize("https://example.com", resp_exm)
-	//fmt.Println(<-size)
-	c.String(http.StatusOK, "ResponseTime of https://golang.org/ is %d \n", <-size)
-	c.String(http.StatusOK, "ResponseTime of https://example.com is %d \n", <-resp_exm)
-	c.String(http.StatusOK, "ResponseTime of https://golang.org/doc is %d ", <-resp_godoc)
+	id := c.Param("id")
+	website := "https://" + id
+	size := make(chan model.WebStats)
+	go responseSize(website, size)
+	c.JSON(200, <-size)
+
 }
